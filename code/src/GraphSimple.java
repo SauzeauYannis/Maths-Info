51eeebb058a1d97679ff46a45fc1f196d04c6d3a
Yannis <sauzeauyannis@gmail.com>
2021-01-07 15:57:55 +0100

    GraphSimple tested and it's work

--- code/src/GraphSimple.java
+++ code/src/GraphSimple.java
@@ -1,87 +1,99 @@
 class GraphSimple {
 
     // Quest a.
-    private final int[][] listesAdjacence;
-    private int[][] matriceAdjacence;
-    private boolean estDefinieListe = false;
-    private boolean estDefinieMatrice = false; 
+    private final int[][] adjacencyListsArray;
+    private int[][] adjacencyMatrix;
+    private boolean isAdjacencyListDefined = false;
+    private boolean isAdjacencyMatrixDefined = false;
 
     public GraphSimple(int n) {
-        this.listesAdjacence = new int[n][];
+        this.adjacencyListsArray = new int[n][];
     }
 
     // Quest b.
     public void setAdjacencyList(int x, int[] neighborhood) {
-        System.arraycopy(neighborhood, 0, this.listesAdjacence[x-1], 0, listesAdjacence.length);
+        this.adjacencyListsArray[x-1] = new int[neighborhood.length];
+        System.arraycopy(neighborhood, 0, this.adjacencyListsArray[x-1], 0, neighborhood.length);
     }
 
     public int[] getAdjacencyList(int x) {
-        return this.listesAdjacence[x-1];
+        return this.adjacencyListsArray[x-1];
     }
 
     // Quest c.
     public int order() {
-        return this.listesAdjacence.length;
+        return this.adjacencyListsArray.length;
     }
 
     public int degree(int x) {
-        return this.listesAdjacence[x].length;
+        return this.adjacencyListsArray[x-1].length;
     }
 
     public boolean isVertex(int x) {
-        return x >= 1 && x <= order(); 
+        return x >= 1 && x <= this.order();
     }
 
     public boolean isEdge(int x, int y) {
-        if (isVertex(x)) {
-            for (int vertex : listesAdjacence[x-1]) {
+        if (isVertex(x) && isVertex(y)) {
+            for (int vertex : this.adjacencyListsArray[x-1]) {
                 if (vertex == y) {
                     return true;
                 }
             }
-        } 
+        }
         return false;
     }
 
     // Quest d.
-    // public void toMatrix(int[][] adjacencyList) {
-    //     if (!estDefinieMatrice) {
-    //         for (int i = 0; i < adjacencyList.length; i++) {
-    //             for (int j = 0; j < adjacencyList[i].length; j++) {
-                    
-    //             }
-    //         }
-    //     }
-    // }
-
-    public void fromMatrix(int[][] matrix) {
-        if (!estDefinieListe) {
-            int n = matrix.length;
-            for (int[] ints : matrix) {
-                int[] buffer = new int[n];
+    private void listToMatrix() {
+        int length = this.adjacencyListsArray.length;
+        this.adjacencyMatrix = new int[length][length];
+        for (int i = 0; i < this.adjacencyMatrix.length; i++) {
+            if (this.adjacencyListsArray[i] != null) {
                 int k = 0;
-                for (int j = 0; j < n; j++) {
-                    if (ints[j] == 1) {
-                        buffer[k] = (j + 1);
+                for (int j = 0; j < this.adjacencyMatrix[i].length; j++) {
+                    if (this.adjacencyListsArray[i][k] == j+1) {
+                        this.adjacencyMatrix[i][j] = 1;
                         k++;
+                    } else {
+                        this.adjacencyMatrix[i][j] = 0;
                     }
                 }
-                int[] neighborhood = new int[k];
-                System.arraycopy(buffer, 0, neighborhood, 0, k);
             }
-            this.estDefinieListe = true;
+        }
+    }
+
+    private void matrixToList() {
+        int length = this.adjacencyMatrix.length;
+        for (int i = 0; i < length; i++) {
+            int[] tmp = new int[length];
+            int k = 0;
+            for (int j = 0; j < length; j++) {
+                if (this.adjacencyMatrix[i][j] == 1) {
+                    tmp[k] = j+1;
+                    k++;
+                }
+            }
+            int[] neighborhood = new int[k];
+            System.arraycopy(tmp, 0, neighborhood, 0, k);
+            this.setAdjacencyList(i+1, neighborhood);
         }
     }
 
     public void setAdjacencyMatrix(int[][] matrix) {
-        this.matriceAdjacence = matrix;
-        if (!estDefinieListe) {
-            fromMatrix(matrix);
+        this.adjacencyMatrix = matrix;
+        this.isAdjacencyMatrixDefined = true;
+        if (!this.isAdjacencyListDefined) {
+            matrixToList();
+            this.isAdjacencyListDefined = true;
         }
-        this.estDefinieMatrice = true;
     }
 
     public int[][] getAdjacencyMatrix() {
-        return this.matriceAdjacence;
+        if (!this.isAdjacencyMatrixDefined) {
+            listToMatrix();
+            this.isAdjacencyMatrixDefined = true;
+        }
+        return this.adjacencyMatrix;
     }
 }
--- code/src/Test.java
+++ code/src/Test.java
@@ -2,9 +2,9 @@ public class Test {
 
     public static void main(String[] args) {
 
-        /*---------------
-         * GraphSimpleIO
-         *---------------*/
+        //////////////////
+        // GraphSimpleIO /
+        //////////////////
 
         // Quest a.
         GraphSimpleIO.Initialize();
@@ -28,6 +28,45 @@ public class Test {
         // Quest g.
         int n = GraphSimpleIO.getInt();
         System.out.println(n);
+
+        ////////////////
+        // GraphSimple /
+        ////////////////
+
+        // Quest a.
+        GraphSimple graphSimple = new GraphSimple(4);
+
+        // Quest b.
+        graphSimple.setAdjacencyList(4, new int[]{1, 2, 4});
+        int [] adjacencyList = graphSimple.getAdjacencyList(4);
+        for (int i: adjacencyList) {System.out.print(i + " ");}
+        System.out.println();
+
+        // Quest c.
+        int order = graphSimple.order();
+        System.out.println("order: " + order);
+
+        int degree = graphSimple.degree(4);
+        System.out.println("degree of 4: " + degree);
+
+        boolean isVertex = graphSimple.isVertex(4);
+        System.out.println("4 is vertex: " + isVertex);
+
+        boolean isEdge = graphSimple.isEdge(4, 3);
+        System.out.println("edge between 4 and 3: " + isEdge);
+        isEdge = graphSimple.isEdge(4, 1);
+        System.out.println("edge between 4 and 1: " + isEdge);
+
+        // Quest d.
+        GraphSimpleIO.printMatrix(graphSimple.getAdjacencyMatrix());
+
+        graphSimple.setAdjacencyMatrix(new int[][]{
+                {0, 1, 0, 1},
+                {1, 1, 0, 1},
+                {0, 1, 1, 1},
+                {0, 0, 0, 0}
+        });
+        GraphSimpleIO.printMatrix(graphSimple.getAdjacencyMatrix());
     }
 
 }
